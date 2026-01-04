f9212e530a7a59a0ef152b1bbc3f78d00eaae936
Ashish Pradhan <ashishpradhan0406@Ashishs-MacBook-Air.local>
2026-01-04 18:50:01 +0530

    Backend ready for deployment

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("CleanAir Hub Backend Running");
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
